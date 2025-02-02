c3609f41492075bc0455edd39a86bb6a7cd7a5b3
Stefan Stefanov <stefan.karolev1@gmail.com>
2025-02-02 21:33:44 +0200

    added floaterm plugin and keybinds

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,19 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+local function toggle_floaterm_with_makeprg()
+  local makeprg_value = vim.api.nvim_get_option_value('makeprg', {})
+  local command = string.format('FloatermSend %s\n', makeprg_value)
+  vim.cmd 'FloatermToggle'
+  vim.cmd(command)
+end
+
+vim.keymap.set('n', '<C-b>', toggle_floaterm_with_makeprg, { noremap = true })
+vim.keymap.set('n', '<C-j>', ':FloatermToggle<CR>', { desc = 'Toggle floaterm' })
+vim.keymap.set('t', '<C-j>', '<C-\\><C-n>:FloatermToggle<CR>', { desc = 'Toggle floaterm' })
+vim.keymap.set('t', '<C-b>', '<C-\\><C-n>:FloatermToggle<CR>', { desc = 'Toggle floaterm' })
+
+return {
+  { 'voldikss/vim-floaterm' },
+}
